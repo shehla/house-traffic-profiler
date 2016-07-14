1b8780c5640570fb47ba8588fb49646bb29758f6
Ubuntu <ubuntu@ip-172-31-14-204.us-west-1.compute.internal>
2016-07-14 01:56:53 +0000

    working

--- googlemaps/delay_controller.py
+++ googlemaps/delay_controller.py
@@ -1,4 +1,5 @@
 import Queue
+import time
 import statistics
 
 
@@ -8,37 +9,47 @@ class DelayController(object):
         self.min_delay = min_delay
         self.max_delay = max_delay
         self.current_delay = min_delay
-        self.queue = Queue()
+        self.queue = Queue.Queue()
         self.max_elements = max_elements
         self.THRESHOLD = THRESHOLD
-        self.STEP = STEP
+        self.run_at = time.time()
 
-    def update_queue(travel_time):
+    #def is_time_to_run(self):
+    #    if time.time() >= self.run_at:
+    #        time_in_mins = get_time(route['from'], route['to'], model)
+    #        self.update_queue(time_in_mins)
+    #        self.run_at += self.current_delay
+    #        return True
+
+    def update_queue(self, travel_time):
         if self.queue.qsize() == self.max_elements:
             # remove the oldest record
             self.queue.get()
         # add the new record
         self.queue.put(travel_time)
+        self.adjust_delay()
 
     def adjust_delay(self):
-        std_dev = statistics.pstdev(list(self.queue))
-        mean = statistics.mean(list(self.queue))
+        std_dev = statistics.pstdev(list(self.queue.queue))
+        mean = statistics.mean(list(self.queue.queue))
         header = '*** DelayController:{0} mean:{1} std:{2} '.format(
             self.route_id,
             mean,
             std_dev,
         )
         if std_dev / mean * 100.0 > self.THRESHOLD:
-            if self.current_delay - self.STEP >= self.min_delay:
-                self.current_delay -= self.STEP
+            if self.current_delay / 2 >= self.min_delay:
+                self.current_delay /= 2.0
                 header = '{0} HIGH ACTIVITY delay:{1}'.format(header, self.current_delay)
             else:
+                self.current_delay = self.min_delay
                 header = '{0} MAXED out delay:{1}'.format(header, self.current_delay)
 
         if std_dev == 0.0:
-            if self.current_delay + self.STEP <= self.max_delay:
-                self.current_delay += self.STEP
+            if self.current_delay * 2.0 <= self.max_delay:
+                self.current_delay *= 2.0
                 header = '{0} low activity delay:{1}'.format(header, self.current_delay)
             else:
+                self.current_delay = self.max_delay
                 header = '{0} NO activity delay:{1}'.format(header, self.current_delay)
         print(header)
--- googlemaps/measure.py
+++ googlemaps/measure.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import time
 
 from googlemaps.route_store import RouteStore
+from googlemaps.delay_controller import DelayController
 
 
 DELAY = float(sys.argv[1])
@@ -29,15 +30,22 @@ def get_time(_from, _to, traffic_model='best_guess'):
 if __name__ == '__main__':
     route_mgr = RouteStore()
     routes = route_mgr.get_routes()
+    delay_controllers = {}
     while True:
         for route in routes:
+            if route['route_id'] not in delay_controllers:
+                delay_controllers[route['route_id']] = DelayController(route['route_id'])
             for model in traffic_models:
                 time_in_mins = get_time(route['from'], route['to'], model)
+                # only use delay for best_guess
+                if model == 'best_guess':
+                    delay_controllers[route['route_id']].update_queue(time_in_mins)
                 rec = {
                     'route_id': route['route_id'],
                     'time': time.time(),
                     'traffic_model': model,
-                    'traffic_time': time_in_mins
+                    'traffic_time': time_in_mins,
+                    'current_delay': delay_controllers[route['route_id']].current_delay,
                 }
 
                 print(json.dumps(rec))
