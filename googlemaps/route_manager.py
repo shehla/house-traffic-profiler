de48b31efe7052fdd02b89555141900e430a82f2
Ubuntu <ubuntu@ip-172-31-14-204.us-west-1.compute.internal>
2016-07-31 16:06:49 +0000

    exception handling

--- googlemaps/route_manager.py
+++ googlemaps/route_manager.py
@@ -9,10 +9,10 @@ gmaps = googlemaps.Client(key='AIzaSyDNIxQQlAu-LzbpCQhvJDMKtPgborYIO7w')
 def get_time(_from, _to, traffic_model='best_guess'):
     # Request directions via public transit
     now = datetime.now()
-    directions_result = gmaps.directions(_from,_to,mode="driving",departure_time=now,traffic_model=traffic_model)
     try:
+        directions_result = gmaps.directions(_from,_to,mode="driving",departure_time=now,traffic_model=traffic_model)
         traffic_time = directions_result[0]['legs'][0]['duration_in_traffic']['text']
-    except KeyError as e:
+    except Exception e:
         print('Nothing found :S {0}'.format(e))
         return None
     if traffic_model == 'best_guess':
--- googlemaps/time_profile_plotter.py
+++ googlemaps/time_profile_plotter.py
@@ -20,7 +20,9 @@ class TimeProfiler(object):
         fig = None
         route_store = RouteStore()
         route = {
-            'route_id': 'umairs_place|broadcom',
+            #'route_id': 'umairs_place|broadcom',
+            #'route_id': 'home|yelp',
+            'route_id': 'yelp|home',
             #'route_id': 'omer|broadcom',
             'from': '3255 Vittori Loop, Dublin',
             'to': '190 Mathilda Ave, Sunnyvale',
@@ -57,11 +59,11 @@ class TimeProfiler(object):
 
         y = utils.sort_list(y, 'epoch')
 
-        fig, graph = plotter.plot_stock_qty(y_p, 'traffic_time', fig=fig, line_type='-', show_xticks=False)
-        fig, graph = plotter.plot_stock_qty(y, 'traffic_time', fig=fig, line_type='-', show_xticks=False)
-        fig, graph = plotter.plot_stock_qty(y_o, 'traffic_time', fig=fig, line_type='-', show_xticks=True)
+        #fig, graph = plotter.plot_stock_qty(y_p, 'traffic_time', fig=fig, line_type='-', show_xticks=False)
+        fig, graph = plotter.plot_stock_qty(y, 'traffic_time', fig=fig, line_type='-', show_xticks=True)
+        #fig, graph = plotter.plot_stock_qty(y_o, 'traffic_time', fig=fig, line_type='-', show_xticks=True)
         start_time = min([r['epoch'] for r in y_p])
-        max_traffic_time = max([r['traffic_time'] for r in y_p])
+        max_traffic_time = max([r['traffic_time'] for r in y])
         addr_str = 'From:{0}  ==>  To:{1}'.format(route['from'], route['to'])
         graph.text(start_time, max_traffic_time * 1.10, addr_str, fontsize=15)
         pl.ylabel('Time in traffic (mins)')
